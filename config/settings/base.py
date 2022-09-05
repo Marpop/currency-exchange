006fdeb44fa12a225ad010e96b170331da6762f0
Marcin Popławski <marpop8@gmail.com>
2022-09-05 23:01:03 +0200

    Add new app

--- config/settings/base.py
+++ config/settings/base.py
@@ -73,7 +73,7 @@ THIRD_PARTY_APPS = [
     "drf_spectacular",
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = ["apps.nbp"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
