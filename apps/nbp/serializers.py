93c7f400c90083452212762aa57f77cebb481bfc
Marcin Popławski <marpop8@gmail.com>
2022-09-08 19:58:46 +0200

    Add serializer

---
+++ apps/nbp/serializers.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from rest_framework import serializers
+
+from apps.nbp.choices import Currency
+
+
+class ExchangeRateSerializer(serializers.Serializer):
+
+    date = serializers.DateField()
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+    currency_input = serializers.ChoiceField(choices=Currency.choices)
+    currency_output = serializers.ChoiceField(choices=Currency.choices)
+
+    class Meta:
+        fields = [
+            "date",
+            "amount",
+            "currency_input",
+            "currency_output",
+        ]
