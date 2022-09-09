4ed084b2ed56a438a6bd5febd5b424b2c54cd8c8
Marcin Popławski <marpop8@gmail.com>
2022-09-09 18:42:14 +0200

    Add NBP API service

---
+++ apps/nbp/services.py
@@ -0,0 +1,29 @@
+import datetime
+import logging
+
+import requests
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class NBP_API:
+    def __init__(self) -> None:
+        self.rates_a_url = "http://api.nbp.pl/api/exchangerates/rates/a"
+        self.logger = logging.getLogger("NBP API")
+        self.timeout = 3
+
+    def get_exchange_response(self, code: str, date: datetime.date) -> Response:
+        try:
+            response = requests.get(
+                url=f"{self.rates_a_url}/{code}/{str(date)}/",
+                timeout=self.timeout,
+                headers={"Content-Type": "application/json"},
+            )
+            if response.status_code == status.HTTP_200_OK:
+                self.logger.info(f"{date} {code} exchange get")
+            else:
+                self.logger.error(f"{date} {code} {response}")
+            return Response(status=response.status_code)
+        except requests.exceptions.RequestException as error:
+            self.logger.error(f"Request error: {error}")
+            return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
---
+++ apps/nbp/tests/test_services.py
@@ -0,0 +1,50 @@
+import datetime
+
+import pytest
+from requests.exceptions import ReadTimeout
+from rest_framework import status
+
+from apps.nbp.choices import CurrencyExchangePLN
+from apps.nbp.services import NBP_API
+
+
+class Test_NBP_API:
+    def setup(self):
+        self.nbp_api = NBP_API()
+        self.code = str(CurrencyExchangePLN.USD)
+        self.date = datetime.date(2016, 4, 4)
+        self.url = f"{self.nbp_api.rates_a_url}/{self.code}/{str(self.date)}/"
+        self.response_ok = {
+            "table": "A",
+            "currency": "dolar amerykański",
+            "code": "USD",
+            "rates": [
+                {
+                    "no": "064/A/NBP/2016",
+                    "effectiveDate": "2016-04-04",
+                    "mid": 3.7254,
+                }
+            ],
+        }
+
+    def test_response_200(self, requests_mock):
+        requests_mock.get(
+            url=self.url, json=self.response_ok, status_code=status.HTTP_200_OK
+        )
+        response = self.nbp_api.get_exchange_response(self.code, self.date)
+        print(response)
+        assert response.data == self.response_ok
+        assert response.status_code == status.HTTP_200_OK
+        requests_mock.call_count == 1
+
+    def test_response_404(self, requests_mock):
+        requests_mock.get(url=self.url, status_code=status.HTTP_404_NOT_FOUND)
+        response = self.nbp_api.get_exchange_response(self.code, self.date)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        requests_mock.call_count == 1
+
+    def test_response_400(self, requests_mock):
+        requests_mock.get(url=self.url, status_code=status.HTTP_400_BAD_REQUEST)
+        response = self.nbp_api.get_exchange_response(self.code, self.date)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        requests_mock.call_count == 1
