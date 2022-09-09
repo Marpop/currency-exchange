c572e467d72cda2ba884712ca8b312cff7fce4a3
Marcin Popławski <marpop8@gmail.com>
2022-09-10 01:33:31 +0200

    Change url name

--- config/api.py
+++ config/api.py
@@ -3,5 +3,9 @@ from django.urls import path
 from apps.nbp.views import ExchangeRateView
 
 urlpatterns = [
-    path("nbp/", ExchangeRateView.as_view(), name="exchange"),
+    path(
+        "nbp/exchange/",
+        ExchangeRateView.as_view(),
+        name="exchange",
+    ),
 ]
