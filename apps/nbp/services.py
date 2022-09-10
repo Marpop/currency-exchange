e76e8da3d731dc03685cc7f3344831b479f550dd
Marcin Popławski <marpop8@gmail.com>
2022-09-10 03:32:37 +0200

    Move bussines logic to services

--- apps/nbp/services.py
+++ apps/nbp/services.py
@@ -1,9 +1,13 @@
 import datetime
 import logging
+from decimal import Decimal
 
 import requests
 from rest_framework import status
 from rest_framework.response import Response
+
+from apps.nbp.choices import Currency
+from apps.nbp.models import ExchangeRatePLN
 
 
 class NBP_API:
@@ -28,11 +32,88 @@ class NBP_API:
             self.logger.error(f"Request error: {error}")
             return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-    def get_exchange_rate(self, code: str, date: datetime.date) -> Response:
+    def _get_exchange_rate(self, code: str, date: datetime.date) -> Response:
         response = self._get_exchange_response(code, date)
         if response.status_code == status.HTTP_200_OK:
             return Response(
                 {"rate": response.data["rates"][0]["mid"]},
                 status=status.HTTP_200_OK,
             )
-        return response
+        return Response(status=response.status_code)
+
+    def _exchange_pln(self, date, currency_input, currency_output, amount):
+        if currency_input == Currency.PLN:
+            rated_currency = currency_output
+        if currency_output == Currency.PLN:
+            rated_currency = currency_input
+
+        try:
+            exchange_rate = ExchangeRatePLN.objects.get(
+                date=date, currency=rated_currency
+            )
+        except ExchangeRatePLN.DoesNotExist:
+            nbp_response = self._get_exchange_rate(rated_currency, date)
+            if nbp_response.status_code == status.HTTP_200_OK:
+                exchange_rate = ExchangeRatePLN.objects.create(
+                    date=date,
+                    currency=rated_currency,
+                    rate=Decimal(nbp_response.data["rate"]),
+                )
+            else:
+                return nbp_response
+
+        if currency_input == Currency.PLN:
+            result = exchange_rate.exchange_to_pln(amount)
+        if currency_output == Currency.PLN:
+            result = exchange_rate.exchange_from_pln(amount)
+
+        return Response(
+            {rated_currency: result},
+            status=status.HTTP_200_OK,
+        )
+
+    def _exchange_not_pln(self, date, currency_input, currency_output, amount):
+        try:
+            exchange_rate_input = ExchangeRatePLN.objects.get(
+                date=date, currency=currency_input
+            )
+        except ExchangeRatePLN.DoesNotExist:
+            nbp_response = self._get_exchange_rate(currency_input, date)
+            if nbp_response.status_code == status.HTTP_200_OK:
+                exchange_rate_input = ExchangeRatePLN.objects.create(
+                    date=date,
+                    currency=currency_input,
+                    rate=Decimal(nbp_response.data["rate"]),
+                )
+            else:
+                return nbp_response
+        try:
+            exchange_rate_output = ExchangeRatePLN.objects.get(
+                date=date, currency=currency_output
+            )
+        except ExchangeRatePLN.DoesNotExist:
+            nbp_response = self._get_exchange_rate(currency_output, date)
+            if nbp_response.status_code == status.HTTP_200_OK:
+                exchange_rate_output = ExchangeRatePLN.objects.create(
+                    date=date,
+                    currency=currency_output,
+                    rate=Decimal(nbp_response.data["rate"]),
+                )
+            else:
+                return nbp_response
+        return Response(
+            {
+                currency_input: exchange_rate_output.exchange_from_pln(
+                    exchange_rate_input.exchange_to_pln(amount)
+                )
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def exchange(self, date, currency_input, currency_output, amount):
+        # one of currencies is PLN
+        if Currency.PLN in (currency_input, currency_output):
+            return self._exchange_pln(date, currency_input, currency_output, amount)
+
+        # both currencies are not PLN
+        return self._exchange_not_pln(date, currency_input, currency_output, amount)
--- apps/nbp/views.py
+++ apps/nbp/views.py
@@ -1,12 +1,8 @@
-from decimal import Decimal
-
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.nbp.choices import Currency
-from apps.nbp.models import ExchangeRatePLN
 from apps.nbp.serializers import ExchangeRateSerializer
 from apps.nbp.services import NBP_API
 
@@ -14,91 +10,14 @@ from apps.nbp.services import NBP_API
 class ExchangeRateView(APIView):
     @extend_schema(parameters=[ExchangeRateSerializer])
     def get(self, request):
-        serializer = ExchangeRateSerializer(
-            data=request.query_params,
-        )
+        serializer = ExchangeRateSerializer(data=request.query_params)
         if serializer.is_valid():
             date = serializer.validated_data["date"]
             currency_input = serializer.validated_data["currency_input"]
             currency_output = serializer.validated_data["currency_output"]
             amount = serializer.validated_data["amount"]
 
-            if currency_input == Currency.PLN:
-                try:
-                    exchange_rate = ExchangeRatePLN.objects.get(
-                        date=date, currency=currency_output
-                    )
-                except ExchangeRatePLN.DoesNotExist:
-                    nbp_response = NBP_API().get_exchange_rate(currency_output, date)
-                    if nbp_response.status_code == status.HTTP_200_OK:
-                        exchange_rate = ExchangeRatePLN.objects.create(
-                            date=date,
-                            currency=currency_output,
-                            rate=Decimal(nbp_response.data["rate"]),
-                        )
-                    else:
-                        return nbp_response
-                return Response(
-                    {currency_output: exchange_rate.exchange_from_pln(amount)},
-                    status=status.HTTP_200_OK,
-                )
-            if currency_output == Currency.PLN:
-                try:
-                    exchange_rate = ExchangeRatePLN.objects.get(
-                        date=date, currency=currency_input
-                    )
-                except ExchangeRatePLN.DoesNotExist:
-                    nbp_response = NBP_API().get_exchange_rate(currency_input, date)
-                    if nbp_response.status_code == status.HTTP_200_OK:
-                        exchange_rate = ExchangeRatePLN.objects.create(
-                            date=date,
-                            currency=currency_input,
-                            rate=Decimal(nbp_response.data["rate"]),
-                        )
-                    else:
-                        return nbp_response
-                return Response(
-                    {currency_input: exchange_rate.exchange_to_pln(amount)},
-                    status=status.HTTP_200_OK,
-                )
+            nbp_api = NBP_API()
 
-            try:
-                exchange_rate_input = ExchangeRatePLN.objects.get(
-                    date=date, currency=currency_input
-                )
-            except ExchangeRatePLN.DoesNotExist:
-                nbp_response = NBP_API().get_exchange_rate(currency_input, date)
-                if nbp_response.status_code == status.HTTP_200_OK:
-                    exchange_rate_input = ExchangeRatePLN.objects.create(
-                        date=date,
-                        currency=currency_input,
-                        rate=Decimal(nbp_response.data["rate"]),
-                    )
-                else:
-                    return nbp_response
-            try:
-                exchange_rate_output = ExchangeRatePLN.objects.get(
-                    date=date, currency=currency_output
-                )
-            except ExchangeRatePLN.DoesNotExist:
-                nbp_response = NBP_API().get_exchange_rate(currency_output, date)
-                if nbp_response.status_code == status.HTTP_200_OK:
-                    exchange_rate_output = ExchangeRatePLN.objects.create(
-                        date=date,
-                        currency=currency_output,
-                        rate=Decimal(nbp_response.data["rate"]),
-                    )
-                else:
-                    return nbp_response
-            return Response(
-                {
-                    currency_input: exchange_rate_output.exchange_from_pln(
-                        exchange_rate_input.exchange_to_pln(amount)
-                    )
-                },
-                status=status.HTTP_200_OK,
-            )
+            return nbp_api.exchange(date, currency_input, currency_output, amount)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-#
