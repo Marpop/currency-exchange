e93d720070045a5b593f5be90041c479fb6e21d1
Marcin Popławski <marpop8@gmail.com>
2022-09-11 21:14:09 +0200

    Add view tests

--- apps/nbp/tests/test_views.py
+++ apps/nbp/tests/test_views.py
@@ -10,25 +10,112 @@ pytestmark = pytest.mark.django_db
 
 class TestExchangeRateView:
     def setup(self):
-        self.url = "api:nbp:exchange"
+        self.url = "{}?date={}&currency_input={}&currency_output={}&amount={}"
+        self.url_test_statuses = self.url.format(
+            reverse("api:nbp:exchange"), "2020-04-04", "USD", "PLN", 100
+        )
         self.api_client = APIClient()
         self.mocker_path = "apps.nbp.services.NBP_API.exchange"
 
-    def test_get_exchange_rate_200(self, mocker):
+    def test_get_200(self, mocker):
         get_exchange_rate = mocker.patch(
             self.mocker_path,
-            return_value=Response({"rate": 3.7254}, status=status.HTTP_200_OK),
+            return_value=Response({"PLN": 250}, status=status.HTTP_200_OK),
         )
-        url = reverse(
-            self.url,
-            kwargs={
-                "date": "2016-04-04",
-                "currency_input": "USD",
-                "currency_output": "PLN",
-                "amount": 2,
-            },
+        response = self.api_client.get(self.url_test_statuses)
+        get_exchange_rate.assert_called_once()
+        assert response.json() == {"PLN": 250}
+        assert response.status_code == status.HTTP_200_OK
+        assert get_exchange_rate.call_count == 1
+
+    def test_get_404(self, mocker):
+        get_exchange_rate = mocker.patch(
+            self.mocker_path,
+            return_value=Response(status=status.HTTP_404_NOT_FOUND),
+        )
+        response = self.api_client.get(self.url_test_statuses)
+        get_exchange_rate.assert_called_once()
+        assert response.json() == {"detail": "Not found."}
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert get_exchange_rate.call_count == 1
+
+    def test_get_400(self, mocker):
+        get_exchange_rate = mocker.patch(
+            self.mocker_path,
+            return_value=Response(status=status.HTTP_400_BAD_REQUEST),
+        )
+        response = self.api_client.get(self.url_test_statuses)
+        get_exchange_rate.assert_called_once()
+        assert response.json() == {"detail": "Not found."}
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert get_exchange_rate.call_count == 1
+
+    def test_get_503(self, mocker):
+        get_exchange_rate = mocker.patch(
+            self.mocker_path,
+            return_value=Response(status=status.HTTP_503_SERVICE_UNAVAILABLE),
+        )
+        response = self.api_client.get(self.url_test_statuses)
+        get_exchange_rate.assert_called_once()
+        assert response.json() == {"detail": "Service unavailable."}
+        assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
+        assert get_exchange_rate.call_count == 1
+
+    @pytest.mark.parametrize(
+        "date, currency_input, currency_output, amount, response_data",
+        [
+            (
+                "2020-04-",
+                "USD",
+                "USD",
+                100,
+                {
+                    "date": [
+                        "Date has wrong format. Use one of these formats instead: YYYY-MM-DD."
+                    ]
+                },
+            ),
+            (
+                "2020-04-04",
+                "USD",
+                "USD",
+                100,
+                {"non_field_errors": ["Currency input and output cannot be the same."]},
+            ),
+            (
+                "2020-04-04",
+                "AAA",
+                "USD",
+                100,
+                {"currency_input": ['"AAA" is not a valid choice.']},
+            ),
+            (
+                "2020-04-04",
+                "USD",
+                "AAA",
+                100,
+                {"currency_output": ['"AAA" is not a valid choice.']},
+            ),
+            (
+                "2020-04-04",
+                "USD",
+                "PLN",
+                "AAA",
+                {"amount": ["A valid number is required."]},
+            ),
+        ],
+    )
+    def test_get_validation(  # pylint: disable=too-many-arguments
+        self,
+        date,
+        currency_input,
+        currency_output,
+        amount,
+        response_data,
+    ):
+        url = self.url.format(
+            reverse("api:nbp:exchange"), date, currency_input, currency_output, amount
         )
         response = self.api_client.get(url)
-        assert response.data == {"PLN": 7.45}
-        assert response.status_code == status.HTTP_200_OK
-        get_exchange_rate.assert_called_once()
+        assert response.json() == response_data
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
--- apps/nbp/views.py
+++ apps/nbp/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.response import Response
@@ -9,15 +11,25 @@ from apps.nbp.services import NBP_API
 
 class ExchangeRateView(APIView):
     @extend_schema(parameters=[ExchangeRateSerializer])
-    def get(self, request):
+    def get(self, request) -> Response:
         serializer = ExchangeRateSerializer(data=request.query_params)
         if serializer.is_valid():
             date = serializer.validated_data["date"]
             currency_input = serializer.validated_data["currency_input"]
             currency_output = serializer.validated_data["currency_output"]
             amount = serializer.validated_data["amount"]
-
-            nbp_api = NBP_API()
-
-            return nbp_api.exchange(date, currency_input, currency_output, amount)
+            nbp = NBP_API()
+            response = nbp.exchange(date, currency_input, currency_output, amount)
+            if response.status_code == status.HTTP_200_OK:
+                return response
+            if response.status_code in [
+                status.HTTP_404_NOT_FOUND,
+                status.HTTP_400_BAD_REQUEST,
+            ]:
+                raise Http404
+            if response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE:
+                return Response(
+                    {"detail": "Service unavailable."},
+                    status=status.HTTP_503_SERVICE_UNAVAILABLE,
+                )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
